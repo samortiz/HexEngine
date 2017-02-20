0963cea7e4c2791e86a3c8e45429ddc7a9470ea1
Sam Ortiz <samortiz@gmail.com>
2017-02-19 20:59:55 -0400

    Fixed pan and zooming, still need to fix bug in zoom center location

--- app/src/main/java/com/alwaysrejoice/hexengine/hexengine/MainActivity.java
+++ app/src/main/java/com/alwaysrejoice/hexengine/hexengine/MainActivity.java
@@ -6,62 +6,30 @@ import android.view.GestureDetector;
 import android.view.MotionEvent;
 import android.view.View;
 
-public class MainActivity extends Activity  implements View.OnTouchListener,  GestureDetector.OnGestureListener {
-    MapView renderView;
+public class MainActivity extends Activity implements View.OnTouchListener {
+  MapView renderView;
 
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        renderView = new MapView(this);
-        renderView.setOnTouchListener(this);
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    renderView = new MapView(this);
+    renderView.setOnTouchListener(this);
+    setContentView(renderView);
+  }
 
-        setContentView(renderView);
-    }
+  protected void onResume() {
+    super.onResume();
+    renderView.resume();
+  }
 
-    protected void onResume() {
-        super.onResume();
-        renderView.resume();
-    }
+  protected void onPause() {
+    super.onPause();
+    renderView.pause();
+  }
 
-    protected void onPause() {
-        super.onPause();
-        renderView.pause();
-    }
+  // OnTouchListener methods
+  public boolean onTouch(View v, MotionEvent event) {
+    renderView.onTouchEvent(event);
+    return true;
+  }
 
-    // OnTouchListener methods
-    public boolean onTouch(View v, MotionEvent event) {
-        switch (event.getAction()) {
-            case MotionEvent.ACTION_UP:
-                renderView.touchUp(event.getX(),event.getY());
-                break;
-
-        }
-        renderView.onTouchEvent(event);
-        return true;
-    }
-
-    // GestureDetector Methods
-    public boolean onDown(MotionEvent e) {
-        return true;
-    }
-
-    public void onShowPress(MotionEvent e) {
-
-    }
-
-    public boolean onSingleTapUp(MotionEvent e) {
-        return false;
-    }
-
-    public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
-        renderView.pan(distanceX, distanceY);
-        return true;
-    }
-
-    public void onLongPress(MotionEvent e) {
-
-    }
-
-    public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
-        return false;
-    }
 }
--- app/src/main/java/com/alwaysrejoice/hexengine/hexengine/MapView.java
+++ app/src/main/java/com/alwaysrejoice/hexengine/hexengine/MapView.java
@@ -8,7 +8,7 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Rect;
-import android.graphics.RectF;
+import android.support.v4.view.MotionEventCompat;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.ScaleGestureDetector;
@@ -20,170 +20,176 @@ import java.io.InputStream;
 import java.util.Random;
 
 public class MapView extends SurfaceView implements Runnable {
-    Bitmap grass;
-    Bitmap tree;
-    Bitmap background;
-    int VIEW_SIZE_X = 1000;
-    int VIEW_SIZE_Y = 1000;
+  // Core screen drawing
+  Thread renderThread = null;
+  SurfaceHolder holder;
+  volatile boolean running = false;
 
-    int BACKGROUND_SIZE_X = 2500;
-    int BACKGROUND_SIZE_Y = 2500;
-    int TILE_SIZE_X = 60;
-    int TILE_SIZE_Y = 60;
+  // Background drawing variables
+  Bitmap grass;
+  Bitmap tree;
+  Bitmap background;
+  int VIEW_SIZE_X = 1000; // size of viewport on the screen
+  int VIEW_SIZE_Y = 1000;
+  int BACKGROUND_SIZE_X = 2500; // size of background map
+  int BACKGROUND_SIZE_Y = 2500;
+  int TILE_SIZE_X = 60; // size of one tile on the map
+  int TILE_SIZE_Y = 60;
+  // Current location of view in the background
+  int mapX = 100;
+  int mapY = 200;
 
-    // Current location of view in the background
-    int viewX = 100;
-    int viewY = 200;
+  // Panning
+  private float gestureStartX = 0f; // Screen location gesture started
+  private float gestureStartY = 0f;
+  private int gestureDiffX = 0; // distance from gestureStartX to current location
+  private int gestureDiffY = 0;
 
-    // Current location touched on the screen
-    float touchX = 100;
-    float touchY = 100;
+  // Scaling
+  private float MIN_SCALE = 0.20f;
+  private float MAX_SCALE = 3.0f;
+  private ScaleGestureDetector mapScaleDetector;
+  private float mapScaleFactor = 1.0f;
+  private float secondFingerX;
+  private float secondFingerY;
 
-    // Scaling
-    private ScaleGestureDetector mapScaleDetector;
-    private float mapScaleFactor = 1.f;
-
-    // Core screen drawing
-    Thread renderThread = null;
-    SurfaceHolder holder;
-    volatile boolean running = false;
-
-    public MapView(Context context) {
-        super(context);
-        holder = getHolder();
-        mapScaleDetector = new ScaleGestureDetector(context, new MapScaleListener());
+  public MapView(Context context) {
+    super(context);
+    holder = getHolder();
+    mapScaleDetector = new ScaleGestureDetector(context, new MapScaleListener());
 
 
-        InputStream inputStream = null;
+    InputStream inputStream = null;
 
+    try {
+      AssetManager assetManager = context.getAssets();
+      inputStream = assetManager.open("grass.png");
+      grass = BitmapFactory.decodeStream(inputStream);
+      inputStream.close();
+      Log.d("BitmapText", "grass.png format: " + grass.getConfig());
+
+      inputStream = assetManager.open("tree.png");
+      BitmapFactory.Options options = new BitmapFactory.Options();
+      tree = BitmapFactory.decodeStream(inputStream, null, options);
+      Log.d("BitmapText", "tree.png format: " + tree.getConfig());
+
+      Random rand = new Random();
+
+      // Make a background map
+      background = Bitmap.createBitmap(BACKGROUND_SIZE_X, BACKGROUND_SIZE_Y, Bitmap.Config.ARGB_8888);
+      Canvas bgCanvas = new Canvas(background);
+      bgCanvas.drawRGB(100, 200, 200);
+      for (int x = 0; x <= BACKGROUND_SIZE_X; x += TILE_SIZE_X) {
+        for (int y = 0; y <= BACKGROUND_SIZE_X; y += TILE_SIZE_Y) {
+          bgCanvas.drawBitmap(grass, x, y, null);
+          if (rand.nextInt(10) > 8) {
+            Paint paint = new Paint();
+            String colorHex = "#" + Integer.toHexString(20 + rand.nextInt(200)) +
+                Integer.toHexString(20 + rand.nextInt(200)) +
+                Integer.toHexString(20 + rand.nextInt(200));
+            Log.d("MapView", "Color:'" + colorHex + "'");
+            paint.setColor(Color.parseColor(colorHex));
+            bgCanvas.drawCircle(x, y, 10, paint);
+          }
+        }
+      }
+
+    } catch (IOException e) {
+      e.printStackTrace();
+    } finally {
+      if (inputStream != null) {
         try {
-            AssetManager assetManager = context.getAssets();
-            inputStream = assetManager.open("grass.png");
-            grass = BitmapFactory.decodeStream(inputStream);
-            inputStream.close();
-            Log.d("BitmapText", "grass.png format: " + grass.getConfig());
-
-            inputStream = assetManager.open("tree.png");
-            BitmapFactory.Options options = new BitmapFactory.Options();
-            tree = BitmapFactory.decodeStream(inputStream, null, options);
-            Log.d("BitmapText", "tree.png format: " + tree.getConfig());
-
-            Random rand = new Random();
-
-            // Make a background map
-            background = Bitmap.createBitmap(BACKGROUND_SIZE_X, BACKGROUND_SIZE_Y, Bitmap.Config.ARGB_8888);
-            Canvas bgCanvas = new Canvas(background);
-            bgCanvas.drawRGB(100, 200, 200);
-            for (int x=0; x<=BACKGROUND_SIZE_X; x+=TILE_SIZE_X) {
-                for (int y=0; y<=BACKGROUND_SIZE_X; y+= TILE_SIZE_Y) {
-                    bgCanvas.drawBitmap(grass, x, y, null);
-                    if (rand.nextInt(10) > 8) {
-                        Paint paint = new Paint();
-                        String colorHex = "#" + Integer.toHexString(20 + rand.nextInt(200)) +
-                                Integer.toHexString(20 + rand.nextInt(200)) +
-                                Integer.toHexString(20 + rand.nextInt(200));
-                        Log.d("MapView", "Color:'" + colorHex + "'");
-                        paint.setColor(Color.parseColor(colorHex));
-                        bgCanvas.drawCircle(x, y, 10, paint);
-                    }
-                }
-            }
-
+          inputStream.close();
         } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            if (inputStream != null) {
-                try {inputStream.close();} catch (IOException e) {}
-            }
         }
+      }
     }
+  }
 
-    public void resume() {
-        Log.d("MapView", "resume");
-        running = true;
-        renderThread = new Thread(this);
-        renderThread.start();
+  public void resume() {
+    Log.d("MapView", "resume");
+    running = true;
+    renderThread = new Thread(this);
+    renderThread.start();
+  }
+
+  public void run() {
+    Log.d("MapView", "running");
+    while (running) {
+      if (!holder.getSurface().isValid()) {
+        Log.d("surface", "Surface is not valid");
+        continue;
+      }
+      Canvas canvas = holder.lockCanvas();
+      canvas.drawRGB(0, 0, 255);
+
+      int viewSize = Math.round(VIEW_SIZE_X * mapScaleFactor);
+      int left = mapX + Math.round(gestureDiffX * mapScaleFactor);
+      int top  = mapY + Math.round(gestureDiffY * mapScaleFactor);
+      int right = mapX + viewSize + Math.round(gestureDiffX * mapScaleFactor);
+      int bottom = mapY + viewSize + Math.round(gestureDiffY * mapScaleFactor);
+      Rect subsetView = new Rect(left, top, right, bottom);
+      Rect scaleView = new Rect(0, 0, VIEW_SIZE_X, VIEW_SIZE_Y);
+
+      canvas.drawBitmap(background, subsetView, scaleView, null);
+
+      canvas.drawBitmap(tree, VIEW_SIZE_X / 2, VIEW_SIZE_Y / 2, null); // center point
+      holder.unlockCanvasAndPost(canvas);
     }
+  }
 
-    public void run() {
-        Log.d("MapView", "running");
-        while (running) {
-            if (!holder.getSurface().isValid()) {
-                Log.d("surface", "Surface is not valid");
-                continue;
-            }
-            Canvas canvas = holder.lockCanvas();
-            canvas.drawRGB(0, 0, 255);
-
-            Rect subsetView = new Rect(viewX, viewY, viewX+VIEW_SIZE_X, viewY+VIEW_SIZE_Y);
-            Rect scaleView = new Rect(0, 0, VIEW_SIZE_X, VIEW_SIZE_Y);
-
-            canvas.drawBitmap(background, subsetView, scaleView, null);
-
-            canvas.drawBitmap(tree, VIEW_SIZE_X/2, VIEW_SIZE_Y/2, null); // center point
-            canvas.drawBitmap(tree, touchX - tree.getWidth() / 2, touchY - tree.getHeight() / 2, null);
-            holder.unlockCanvasAndPost(canvas);
-        }
+  public void pause() {
+    Log.d("MapView", "pause");
+    running = false;
+    while (true) {
+      try {
+        renderThread.join();
+        return;
+      } catch (InterruptedException e) {
+        // retry
+      }
     }
+  }
 
-    public void pause() {
-        Log.d("MapView", "pause");
-        running = false;
-        while(true) {
-            try {
-                renderThread.join();
-                return;
-            } catch (InterruptedException e) {
-                // retry
-            }
-        }
+
+  @Override
+  public boolean onTouchEvent(MotionEvent event) {
+    mapScaleDetector.onTouchEvent(event);
+    int action = MotionEventCompat.getActionMasked(event);
+    int index = MotionEventCompat.getActionIndex(event);
+    float newX = event.getX(index);
+    float newY = event.getY(index);
+
+    // First finger down (start gesture)
+    if (action == MotionEvent.ACTION_DOWN) {
+      gestureStartX = newX;
+      gestureStartY = newY;
+
+    // Moving / dragging a finger
+    } else if (action == MotionEvent.ACTION_MOVE) {
+      gestureDiffX = Math.round(gestureStartX - newX);
+      gestureDiffY = Math.round(gestureStartY - newY);
+
+    // Releasing the last finger
+    } else if (action == MotionEvent.ACTION_UP) {
+      mapX = mapX + Math.round((gestureStartX - newX) * mapScaleFactor);
+      mapY = mapY + Math.round((gestureStartY - newY) * mapScaleFactor);
+      gestureDiffX = 0; // clear the diff, the mapX has moved now
+      gestureDiffY = 0;
     }
+    return false;
+  }
 
-    public void touchUp(float x, float y) {
-        this.touchX = x;
-        this.touchY = y;
-
-        int newX = viewX + (Math.round(x) - (VIEW_SIZE_X / 2));
-        int newY = viewY + (Math.round(y) - (VIEW_SIZE_Y /2));
-
-        // Bounds checking
-        if (newX < 0) newX = 0;
-        if (newY < 0) newY = 0;
-        if (newX > BACKGROUND_SIZE_X - VIEW_SIZE_X) {
-            newX = BACKGROUND_SIZE_X - VIEW_SIZE_X;
-        }
-        if (newY > BACKGROUND_SIZE_Y - VIEW_SIZE_Y) {
-            newY = BACKGROUND_SIZE_Y - VIEW_SIZE_Y;
-        }
-
-        this.viewX = newX;
-        this.viewY = newY;
-        Log.d("touch", "at "+viewX+","+viewY);
-    }
-
-    public void pan(float x, float y) {
-        Log.d("gesture", "pan "+x+","+y);
-    }
-
-
-
+  private class MapScaleListener extends ScaleGestureDetector.SimpleOnScaleGestureListener {
     @Override
-    public boolean onTouchEvent(MotionEvent event) {
-        mapScaleDetector.onTouchEvent(event);
-        Log.d("Touch", "onTouchEvent");
-        return false;
-    }
+    public boolean onScale(ScaleGestureDetector detector) {
+      mapScaleFactor = mapScaleFactor * (1f - (detector.getScaleFactor() - 1f));
+      mapScaleFactor = Math.max(MIN_SCALE, Math.min(mapScaleFactor, MAX_SCALE));
 
-    private class MapScaleListener extends ScaleGestureDetector.SimpleOnScaleGestureListener {
-        @Override
-        public boolean onScale(ScaleGestureDetector detector) {
-            mapScaleFactor *= detector.getScaleFactor();
-            // Don't let the object get too small or too large.
-            mapScaleFactor = Math.max(0.1f, Math.min(mapScaleFactor, 5.0f));
-            invalidate();
-            Log.d("Scale", "Scaling to "+mapScaleFactor);
-            return true;
-        }
+      invalidate();
+      Log.d("Scale", "Scaling to " + mapScaleFactor + " scaleFactor=" + detector.getScaleFactor());
+      return true;
     }
+  }
 }
 
